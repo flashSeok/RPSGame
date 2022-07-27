74e80f06ef33bb6eed7e20115076627fc42e3c0f
Henry Kim <flashSeok@gmail.com>
2022-07-27 17:10:15 +0900

    "앱 올리기"

---
+++ RPSGame/Rps.swift
@@ -0,0 +1,15 @@
+//
+//  Rps.swift
+//  RPSGame
+//
+//  Created by 김현석 on 2022/07/27.
+//
+
+import Foundation
+
+// 원시값(raw value)
+enum Rps: Int {
+    case rock // 0
+    case paper // 1
+    case scissors // 2
+}
